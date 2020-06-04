cf5dd0ba32c70f54b6f3ad1be8a44d79d500f7de
mingyang.tan <mingyang.tan@mobvista.com>
2020-06-04 15:28:06 +0800

    remove react-router-redux

--- src/containers/frame/Frame.jsx
+++ src/containers/frame/Frame.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { FormattedMessage } from "react-intl";
-import { DatePicker } from "antd";
+import { DatePicker, Upload, Button, message } from "antd";
 
 class Frame extends React.Component {
   constructor(props) {
@@ -10,10 +10,31 @@ class Frame extends React.Component {
   }
 
   render() {
+    const props = {
+      name: "file",
+      action: "http://localhost:3001/files/postFile",
+      headers: {
+        authorization: "authorization-text",
+      },
+      onChange(info) {
+        if (info.file.status !== "uploading") {
+          console.log(info.file, info.fileList);
+        }
+        if (info.file.status === "done") {
+          message.success(`${info.file.name} file uploaded successfully`);
+        } else if (info.file.status === "error") {
+          message.error(`${info.file.name} file upload failed.`);
+        }
+      },
+    };
+
     return (
       <div>
         <FormattedMessage id="guide.tips" />
         <DatePicker />
+        <Upload {...props}>
+          <Button>Click to Upload</Button>
+        </Upload>
       </div>
     );
   }
